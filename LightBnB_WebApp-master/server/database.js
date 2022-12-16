d479418e9ef45d69d409b8867fcd503d73c5b297
larsyaeger <larsyaeger@gmail.com>
2022-12-15 17:18:31 -0700

    refactored getAllReservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -92,7 +92,7 @@ const addUser =  function(user) {
       RETURNING *`, [user.name, user.email, user.password]
     )
     .then((result) => {
-      console.log(result.rows)
+      //console.log(result.rows)
       return result.rows[0];
     })
     .catch((err) => {
@@ -106,14 +106,35 @@ const addUser =  function(user) {
 exports.addUser = addUser;
 
 /// Reservations
-
+//victoriablackwell@outlook.com
 /**
  * Get all reservations for a single user.
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  
+  return pool
+    .query(`
+      SELECT reservations.*, properties.*, avg(rating) as average_rating
+      FROM reservations
+      JOIN properties ON reservations.property_id = properties.id
+      JOIN property_reviews ON properties.id = property_reviews.property_id
+      WHERE reservations.guest_id = $1
+      GROUP BY properties.id, reservations.id
+      ORDER BY reservations.start_date
+      LIMIT $2`, [guest_id, limit]
+    )
+    .then((result) => {
+      console.log('result.rows', result.rows)
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log('err.message', err.message);
+    });
+
+  
+  //return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
